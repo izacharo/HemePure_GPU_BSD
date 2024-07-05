14e8d7241c442d08060bb4f4c2d55383dd864617
Ioannis Zacharoudiou <i.zacharoudiou@gmail.com>
2024-07-05 11:59:27 +0300

    Remove dep/build

--- dep/build/CTemplate-prefix/src/CTemplate/statemachine.c
+++
@@ -1,1 +0,0 @@
-src/htmlparser/statemachine.cc
--- dep/build/ParMETIS-prefix/src/ParMETIS/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/gkregex.c.o.d
+++
@@ -1,3 +0,0 @@
-libmetis/CMakeFiles/metis.dir/__/GKlib/gkregex.c.o: \
- /home/ioannis/Documents/UCL_project/code_development/GPU_code/github_GPU_Hemepure_GPU_BSD/HemePure_GPU_BSD/dep/build/ParMETIS-prefix/src/ParMETIS/metis/GKlib/gkregex.c \
- /usr/include/stdc-predef.h
